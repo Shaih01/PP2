e4547af4d1e7b7a72865150ee8c31b9de922c297
Shaih01 <44065633+Shaih01@users.noreply.github.com>
2019-02-07 18:30:03 +0600

    comment

---
+++ week 2/Task4/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "C:/Top-level folder/path/t.txt";
+            string path1 = "C:/Top-level folder/path1/t.txt";
+            FileInfo f = new FileInfo(path);
+
+            if(f.Exists)
+            {
+                f.CopyTo(path1);
+                f.Delete();
+            }
+        }
+    }
+}
