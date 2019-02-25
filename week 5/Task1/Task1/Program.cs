b8e5d4465fdfb8459a5371c596c6a02e19ec8f69
Shaih01 <44065633+Shaih01@users.noreply.github.com>
2019-02-25 14:26:20 +0600

    WEEK5

---
+++ week 5/Task1/Task1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+namespace Task1
+{
+    public class Complex_number
+    {
+        public int a;
+        public int b;
+
+        public Complex_number() { }
+        public Complex_number(int a, int b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+        
+        public override string ToString()
+        {   
+            return a + "/" + b;
+        }
+        public void Save(Complex_number cn)
+        {
+            FileStream fs = new FileStream("complex_numbers.xml", FileMode.Truncate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex_number));
+            xs.Serialize(fs, cn);
+            fs.Close();
+        }
+        public static Complex_number operator +(Complex_number a,Complex_number b)
+        {
+            Complex_number c = new Complex_number();
+            c.a = a.a + b.a;
+            c.b = a.b + b.b;
+            return c;
+        }
+        public static Complex_number operator -(Complex_number a, Complex_number b)
+        {
+            Complex_number c = new Complex_number();
+            c.a = a.a - a.b;
+            c.b = a.b - b.b;
+            
+            return c;
+        }
+        public static Complex_number operator *(Complex_number a, Complex_number b)
+        {
+            Complex_number c = new Complex_number();
+            c.a = a.a * a.b;
+            c.b = a.b * b.b;
+            return c;
+        }
+        public static Complex_number operator /(Complex_number a, Complex_number b)
+        {
+            Complex_number c = new Complex_number();
+            c.a = a.a / b.b;
+            c.b = a.b / b.a;
+            return c;
+        }
+    }
+
+    class Program
+    {
+        public static void F1()
+        {
+            Complex_number cn = new Complex_number(12, 3);
+            FileStream fs = new FileStream("complex_numbers.xml", FileMode.Truncate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex_number));
+            xs.Serialize(fs, cn + cn);
+            Console.WriteLine(cn + cn);
+            fs.Close();
+        }
+
+        public static void F2()
+        {
+            FileStream fs = new FileStream("complex_numbers.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Complex_number));
+            Complex_number cn1 = xs.Deserialize(fs) as Complex_number;
+            Console.WriteLine(cn1 + cn1);
+            fs.Close();
+        }
+        
+        static void Main(string[] args)
+        {
+            F1();
+            //F2();
+        }
+    }
+}
---
+++ week 5/Task2/Task2/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Task2
+{
+    public class Mark
+    {
+        public int points;
+        public string grade;
+
+        public Mark() { }
+
+        public Mark(int points)
+        {
+            if (this.points >= 0 && this.points <= 100)
+                this.points = points;
+            else
+                this.points = 0;
+        }
+
+        public void GetLetter()
+        {
+            if (points <= 100 && points >= 95)
+            {
+                grade = "A";
+            }
+            if (points <= 94 && points >= 90)
+            {
+                grade = "A-";
+            }
+            if (points <= 89 && points >= 85)
+            {
+                grade = "B+";
+            }
+            if (points <= 84 && points >= 80)
+            {
+                grade = "B";
+            }
+            if (points <= 79 && points >= 75)
+            {
+                grade = "B-";
+            }
+            if (points <= 74 && points >= 70)
+            {
+                grade = "C+";
+            }
+            if (points <= 69 && points >= 65)
+            {
+                grade = "C";
+            }
+            if (points <= 64 && points >= 60)
+            {
+                grade = "C-";
+            }
+            if (points <= 59 && points >= 55)
+            {
+                grade = "D+";
+            }
+            if (points <= 54 && points >= 50)
+            {
+                grade = "D";
+            }
+            if (points < 50)
+            {
+                grade = "F";
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Your points: " + points + "." + " Your grade is " + grade;
+        }
+
+    }
+
+    class Program
+    {
+        public static void F1()
+        {
+            Mark m = new Mark(97);
+            FileStream fs = new FileStream("Mark.xml", FileMode.Truncate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Mark));
+            xs.Serialize(fs, m);
+            m.GetLetter();
+            Console.WriteLine(m);
+            fs.Close();
+        }
+
+        public static void F2()
+        {
+            FileStream fs = new FileStream("Mark.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            XmlSerializer xs = new XmlSerializer(typeof(Mark));
+            Mark m1 = xs.Deserialize(fs) as Mark;
+            Console.WriteLine(m1);
+            fs.Close();
+        }
+
+        public static void Main(string[] args)
+        {
+            F1();
+            //F2();
+        }
+    }
+}
